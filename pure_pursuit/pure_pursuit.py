86ea799ba54884f496b490f7d48ecc445aa1e9b2
Gaurav Kuppa <gkuppa@ghostrobotics.io>
2023-04-30 23:12:43 -0400

    cleaned up

--- pure_pursuit/pure_pursuit.py
+++ pure_pursuit/pure_pursuit.py
@@ -25,16 +25,13 @@ class PurePursuit:
     def __init__(self, waypoints):
         self.is_clockwise = False
 
-        self._waypoints = np.array([waypoints.x, waypoints.y]).T
+        self.waypoints = np.array([waypoints.x, waypoints.y]).T
         self.numWaypoints = self.waypoints.shape[0]
         self.ref_speed = waypoints.v
 
         self.L = 1.5
         self.steering_gain = 0.5
 
-    @property
-    def waypoints(self):
-        return self._waypoints
     
     def get_target_waypoint(self, obs, agent):
         # Get current pose
@@ -53,8 +50,6 @@ class PurePursuit:
         return targetPoint, target_point_index
 
     def control(self, obs, agent, offset=None):
-        print("---")
-        print("agent", agent)
         # Get current pose
         self.currX = obs['poses_x'][agent - 1]
         self.currY = obs['poses_y'][agent - 1]
@@ -63,19 +58,13 @@ class PurePursuit:
         # Find closest waypoint to where we are
         self.distances = distance.cdist(self.currPos, self.waypoints, 'euclidean').reshape((self.numWaypoints,))
         self.closest_index = np.argmin(self.distances)
-        # print("closest_index", self.closest_index)
 
-        # print("offset", offset)
         # Find target point
         targetPoint, target_point_index = self.get_closest_point_beyond_lookahead_dist(self.L)
 
-        print(targetPoint)
-        # print(f"agent {agent} target point {targetPoint} offset {offset}")
-        # assert targetPoint.tolist() in self.waypoints.tolist(), f"{targetPoint}"
-        # targetPoint = targetPoint + offset if agent == 1 else targetPoint
         if isinstance(offset, np.ndarray):  # agent == 1:
             targetPoint = targetPoint + offset  # += is not overwritten by np!
-        print(targetPoint)
+
         # calculate steering angle / curvature
         waypoint_y = np.dot(np.array([np.sin(-obs['poses_theta'][agent - 1]), np.cos(-obs['poses_theta'][agent - 1])]),
                             targetPoint - np.array([self.currX, self.currY]))
@@ -92,7 +81,6 @@ class PurePursuit:
 
     def get_closest_point_beyond_lookahead_dist(self, threshold):
         point_index = self.closest_index
-        print("closest_index =", self.closest_index)
         dist = self.distances[point_index]
 
         while dist < threshold:
@@ -107,9 +95,6 @@ class PurePursuit:
                     point_index = 0
                 dist = self.distances[point_index]
 
-        print("point_index =", point_index)
         point = self.waypoints[point_index]
-        # print("point =", point)
-        # print("waypoint[44] =", self.waypoints[44])
-
+        
         return point, point_index
